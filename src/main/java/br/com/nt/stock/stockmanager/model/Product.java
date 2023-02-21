374be6d6ab7cbe3c6ed1bcab98ec1d3f48d4634a
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-21 18:47:08 -0300

    creating connection with hikari

---
+++ src/main/java/br/com/nt/stock/stockmanager/model/Product.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package br.com.nt.stock.stockmanager.model;
+
+/**
+ * @author Neto
+ *
+ */
+public class Product {
+
+	private Long id;
+	private String name;
+	private int amount;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+}
---
+++ src/main/java/br/com/nt/stock/stockmanager/util/config/DatabaseConfig.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package br.com.nt.stock.stockmanager.util.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Neto
+ *
+ */
+@Configuration
+public class DatabaseConfig {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Value("${spring.datasource.username}")
+	private String dbUsername;
+
+	@Value("${spring.datasource.password}")
+	private String dbPassword;
+
+	@Value("${spring.datasource.poolSize}")
+	private int maximumPoolSize;
+
+	@Value("${spring.datasource.timeout}")
+	private Long timeOut;
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		config.setUsername(dbUsername);
+		config.setPassword(dbPassword);
+		config.setMaximumPoolSize(maximumPoolSize);
+		config.setConnectionTimeout(timeOut);
+		return new HikariDataSource(config);
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+}
