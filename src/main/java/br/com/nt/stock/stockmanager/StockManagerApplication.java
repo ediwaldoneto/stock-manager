9da4082f2b6f154c991f392361c0d74859a96d61
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-21 18:00:10 -0300

    start of adaptations

--- src/main/java/br/com/nt/stock/stockmanager/StockManagerApplication.java
+++ src/main/java/br/com/nt/stock/stockmanager/StockManagerApplication.java
@@ -2,8 +2,19 @@ package br.com.nt.stock.stockmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class
+})
+@ComponentScan(basePackages = {"br.com.nt.stock.stockmanager.*"})
 public class StockManagerApplication {
 
 	public static void main(String[] args) {
