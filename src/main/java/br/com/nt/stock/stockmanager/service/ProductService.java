9287a78b6532141f48ec980d7dd3c0941904aa89
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-25 08:50:11 -0300

    add service

---
+++ src/main/java/br/com/nt/stock/stockmanager/service/ProductService.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package br.com.nt.stock.stockmanager.service;
+
+import java.util.List;
+
+import br.com.nt.stock.stockmanager.model.Product;
+
+/**
+ * @author Neto
+ *
+ */
+public interface ProductService {
+
+	/**
+	 * 
+	 * @param id
+	 * @return user
+	 */
+	public Product findById(final Long id);
+
+	/**
+	 * 
+	 * @return List of all users
+	 */
+	public List<Product> findAll();
+
+	/**
+	 * 
+	 * @param product
+	 */
+	public void save(final Product product);
+
+	/**
+	 * 
+	 * @param product
+	 */
+	public void update(final Product product);
+
+	/**
+	 * 
+	 * @param id
+	 */
+	public void delete(final Long id);
+
+}
