d7b97079b515283ab09d883e85bbc17787eed02a
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-26 23:34:13 -0300

    Continuing controller implementations

--- src/main/java/br/com/nt/stock/stockmanager/controller/ProductController.java
+++ src/main/java/br/com/nt/stock/stockmanager/controller/ProductController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,10 @@ import br.com.nt.stock.stockmanager.dto.model.ProductDTO;
 import br.com.nt.stock.stockmanager.dto.reponse.Response;
 import br.com.nt.stock.stockmanager.model.Product;
 import br.com.nt.stock.stockmanager.service.ProductService;
+import jakarta.validation.Valid;
 
 /**
- * @author Neto
+ * @author Neto	
  *
  */
 @RestController
@@ -56,5 +59,30 @@ public class ProductController {
 		}
 		return new ResponseEntity<>(response, HttpStatus.OK);
 
+	}
+	/**
+	 * 
+	 * @param dto
+	 * @return
+	 * 
+	 * 
+	 * 201 - Created: Everything worked as expected.
+	 * 400 - Bad Request: The request was unacceptable, often due to missing a required parameter.
+	 * 404 - Not Found: The requested resource doesn't exist.
+	 * 409 - Confli* 201 - Created: Everything worked as expected.ct: The request conflicts with another request (perhaps due to using the same idempotent key).
+	 * 429 - Too Many Requests: Too many requests hit the API too quickly. We recommend an exponential back-off of your requests.
+	 * 500, 502, 503, 504 - Server Errors: something went wrong on API end (These are rare).
+	 */
+	@PostMapping
+	public ResponseEntity<Response<ProductDTO>> create(@RequestBody @Valid final ProductDTO dto){
+		
+		Response<ProductDTO> response = new Response<>();
+		
+		Product product = dto.convertDTOToEntity();
+		productService.save(product);
+		response.setData(dto);
+		
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+		
 	}
 }
