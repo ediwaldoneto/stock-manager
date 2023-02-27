f2fa53ab6f34e9b18f19ee2faaec7c8172b81551
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-26 22:56:44 -0300

    controller

--- src/main/java/br/com/nt/stock/stockmanager/controller/ProductController.java
+++ src/main/java/br/com/nt/stock/stockmanager/controller/ProductController.java
@@ -29,8 +29,20 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
-
 	
+    /**
+     * 
+     * @param id
+     * @return ResponseEntity with a Response ProductDTO object and the HTTP status
+     * 
+     * 
+     * 200 - OK: Everything worked as expected.
+	 * 400 - Bad Request: The request was unacceptable, often due to missing a required parameter.
+	 * 404 - Not Found: The requested resource doesn't exist.
+	 * 409 - Conflict: The request conflicts with another request (perhaps due to using the same idempotent key).
+	 * 429 - Too Many Requests: Too many requests hit the API too quickly. We recommend an exponential back-off of your requests.
+	 * 500, 502, 503, 504 - Server Errors: something went wrong on API end (These are rare).
+     */
 	@GetMapping("/{id}")
 	public ResponseEntity<Response<ProductDTO>> findById(@PathVariable("id") final Long id) {
 
@@ -41,12 +53,7 @@ public class ProductController {
 			ModelMapper modelMapper = new ModelMapper();
 			ProductDTO dto = modelMapper.map(product.get(), ProductDTO.class);
 			response.setData(dto);
-		} else {
-			/**
-			 * here it will be implemented if the product is not found
-			 */
 		}
-
 		return new ResponseEntity<>(response, HttpStatus.OK);
 
 	}
