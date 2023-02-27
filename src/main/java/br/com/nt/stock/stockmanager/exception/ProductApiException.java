d8afb0f5031e909f99aa4c6a40c7bb44e96ee02b
ediwaldo.neto <ediwaldo.neto@outlook.com>
2023-02-27 17:15:54 -0300

    Error Treatments

--- src/main/java/br/com/nt/stock/stockmanager/exception/ProductApiException.java
+++ src/main/java/br/com/nt/stock/stockmanager/exception/ProductApiException.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ServerErrorException;
 
 import br.com.nt.stock.stockmanager.dto.reponse.Response;
@@ -44,5 +45,20 @@ public class ProductApiException<T> {
 		response.addErrorToResponse(exception.getLocalizedMessage());
 
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+	}
+
+	/**
+	 * 
+	 * @param exception
+	 * @return ResponseEntity<Response<T>>
+	 */
+	@ExceptionHandler(value = { MethodArgumentTypeMismatchException.class })
+	protected ResponseEntity<Response<T>> handleAPIMethodArgumentTypeMismatchException(
+			MethodArgumentTypeMismatchException exception) {
+
+		Response<T> response = new Response<>();
+		response.addErrorToResponse(exception.getLocalizedMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 	}
 }
